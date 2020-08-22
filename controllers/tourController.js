4678a5e937bde67582d0b60682770410c9c16cbe
Darrell Pawson <dpawson905@gmail.com>
2020-08-22 16:49:01 -0400

    add error handler to routes that look for an item by id

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -3,6 +3,7 @@
 const Tour = require('../models/tourModel');
 const APIFeatures = require('../utils/apiFeatures');
 const catchAsync = require('../utils/catchAsync');
+const AppError = require('../utils/appError');
 
 exports.aliasTopTours = (req, res, next) => {
   req.query.limit = '5';
@@ -30,6 +31,10 @@ exports.getAllTours = catchAsync(async (req, res, next) => {
 
 exports.getTour = catchAsync(async (req, res, next) => {
   const tour = await Tour.findById(req.params.id);
+
+  if (!tour) {
+    return next(new AppError('No tour found with that id', 404));
+  }
   res.status(200).json({
     status: 'success',
     data: {
@@ -58,6 +63,9 @@ exports.updateTour = catchAsync(async (req, res, next) => {
     // This has to be used to run a custom validator in the model file
     context: 'query',
   });
+  if (!tour) {
+    return next(new AppError('No tour found with that id', 404));
+  }
   res.status(200).json({
     status: 'success',
     data: {
@@ -67,7 +75,10 @@ exports.updateTour = catchAsync(async (req, res, next) => {
 });
 
 exports.deleteTour = catchAsync(async (req, res, next) => {
-  await Tour.findByIdAndDelete(req.params.id);
+  const tour = await Tour.findByIdAndDelete(req.params.id);
+  if (!tour) {
+    return next(new AppError('No tour found with that id', 404));
+  }
   res.status(204).json({
     status: 'success',
     data: null,
