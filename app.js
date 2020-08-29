e270b33c5c3a2363864b06c6592c969edb5112fc
Darrell Pawson <dpawson905@gmail.com>
2020-08-29 15:53:35 -0400

    work on front end layouts

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const path = require('path');
 const express = require('express');
 const morgan = require('morgan');
 const rateLimit = require('express-rate-limit');
@@ -13,6 +14,9 @@ const userRouter = require('./routes/userRoutes');
 const reviewRouter = require('./routes/reviewRoutes');
 
 const app = express();
+app.set('view engine', 'pug');
+app.set('views', path.join(__dirname, 'views'));
+app.use(express.static(path.join(__dirname, 'public')));
 
 // 1) GLOBAL MIDDLEWARES
 // Dev logging
@@ -55,8 +59,6 @@ app.use(
   })
 );
 
-app.use(express.static(`${__dirname}/public`));
-
 // Test middleware
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
@@ -64,6 +66,26 @@ app.use((req, res, next) => {
 });
 
 // 3) ROUTES
+app.get('/', (req, res, next) => {
+  res.status(200).render('base', {
+    title: 'Exciting tours for adventurous people',
+    tour: 'The Forest Hiker',
+    user: 'Darrell',
+  });
+});
+
+app.get('/overview', (req, res, next) => {
+  res.status(200).render('overview', {
+    title: 'All Tours',
+  });
+});
+
+app.get('/tour', (req, res, next) => {
+  res.status(200).render('tour', {
+    title: 'The Forest Hiker Tour',
+  });
+});
+
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 app.use('/api/v1/reviews', reviewRouter);
